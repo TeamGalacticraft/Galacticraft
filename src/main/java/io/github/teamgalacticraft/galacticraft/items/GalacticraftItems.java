40f65d0bd604aae24e1e84d0b9a8de7d7987e4ae
Joe van der Zwet <me@joezwet.me>
2019-03-18 20:01:16 +1300

    Add Items itemGroup
    Signed-off-by: Joe van der Zwet <me@joezwet.me>

--- src/main/java/io/github/teamgalacticraft/galacticraft/items/GalacticraftItems.java
+++ src/main/java/io/github/teamgalacticraft/galacticraft/items/GalacticraftItems.java
@@ -1,7 +1,21 @@
 package io.github.teamgalacticraft.galacticraft.items;
 
+import io.github.teamgalacticraft.galacticraft.Constants;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 /**
  * @author <a href="https://github.com/teamgalacticraft">TeamGalacticraft</a>
  */
 public class GalacticraftItems {
+
+    public static ItemGroup ITEMS_GROUP = FabricItemGroupBuilder.create(
+            new Identifier(Constants.MOD_ID, Constants.Items.ITEM_GROUP))
+            .build();
+
+    public static void init() {
+
+    }
 }
