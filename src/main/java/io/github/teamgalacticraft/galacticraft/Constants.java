ff62b8dea91c05d34dc7c546be680fab78dffb92
Joe van der Zwet <me@joezwet.me>
2019-03-19 21:20:00 +1300

    Add block constants
    Signed-off-by: Joe van der Zwet <me@joezwet.me>

--- src/main/java/io/github/teamgalacticraft/galacticraft/Constants.java
+++ src/main/java/io/github/teamgalacticraft/galacticraft/Constants.java
@@ -1,7 +1,5 @@
 package io.github.teamgalacticraft.galacticraft;
 
-import org.lwjgl.system.CallbackI;
-
 /**
  * @author <a href="https://github.com/teamgalacticraft">TeamGalacticraft</a>
  */
@@ -11,8 +9,9 @@ public class Constants {
 
     // Blocks
     public static class Blocks {
+        public static final String ITEM_GROUP = "blocks";
+
         // Natural
-        public static final String ITEM_GROUP = "block";
         public static final String MOON_TURF = "moon_turf_block";
         public static final String MOON_ROCK = "moon_rock_block";
         public static final String MOON_DIRT = "moon_dirt_block";
@@ -24,21 +23,155 @@ public class Constants {
         public static final String MARS_DUNGEON_BRICK_BLOCK = "mars_dungeon_brick_block";
         public static final String DENSE_ICE_BLOCK = "dense_ice_block";
         public static final String ASTEROID_ROCK_BLOCK = "asteroid_rock_block";
+        public static final String ASTEROID_ROCK_BLOCK_1 = "asteroid_rock_block_1";
+        public static final String ASTEROID_ROCK_BLOCK_2 = "asteroid_rock_block_2";
         public static final String VENUS_ROCK_BLOCK = "venus_rock_block";
+        public static final String VENUS_ROCK_BLOCK_1 = "venus_rock_block_1";
+        public static final String VENUS_ROCK_BLOCK_2 = "venus_rock_block_2";
+        public static final String VENUS_ROCK_BLOCK_3 = "venus_rock_block_3";
+        public static final String VENUS_ROCK_BLOCK_SCORCHED = "venus_rock_block_scorched";
         public static final String VOLCANIC_ROCK_BLOCK = "volcanic_rock_block";
         public static final String SCORCHED_ROCK_BLOCK = "scorched_rock_block";
         public static final String PUMICE_BLOCK = "pumice_block";
         public static final String VAPOR_SPOUT_BLOCK = "vapor_spout_block";
-        public static final String TIN_DECORATION_BLOCK = "tin_decoration_block";
-        public static final String TIN_WALL_BLOCK = "tin_wall_block";
-        public static final String COPPER_ORE_BLOCK = "copper_ore_block";
-        public static final String TIN_ORE_BLOCK = "tin_ore_block";
+
+        // Ore
         public static final String ALUMINUM_ORE_BLOCK = "aluminum_ore_block";
+        public static final String ASTEROID_ALUMINUM_ORE_BLOCK = "asteroid_aluminum_ore_block";
+        public static final String CHEESE_ORE_BLOCK = "cheese_ore_block";
+        public static final String COPPER_ORE_BLOCK = "copper_ore_block";
+        public static final String MOON_COPPER_ORE_BLOCK = "moon_copper_ore_block";
+        public static final String MARS_COPPER_ORE_BLOCK = "mars_copper_ore_block";
+        public static final String DESH_ORE_BLOCK = "desh_ore_block";
+        public static final String ILMENITE_ORE_BLOCK = "ilmenite_ore_block";
+        public static final String MARS_IRON_ORE_BLOCK = "mars_iron_ore_block";
+        public static final String ASTEROID_IRON_ORE_BLOCK = "asteroid_iron_ore_block";
         public static final String SILICON_ORE_BLOCk = "silicon_ore_block";
+        public static final String TIN_ORE_BLOCK = "tin_ore_block";
+        public static final String MOON_TIN_ORE_BLOCK = "moon_tin_ore_block";
+        public static final String MARS_TIN_ORE_BLOCK = "mars_tin_ore_block";
+        public static final String GALENA_ORE_BLOCK = "galena_ore_block";
+
+        // Solid Blocks
+        public static final String ALUMINUM_BLOCK = "aluminum_block";
         public static final String COPPER_BLOCK = "copper_block";
         public static final String TIN_BLOCK = "tin_block";
-        public static final String ALUMINUM_BLOCK = "aluminum_block";
         public static final String SILICON_BLOCK = "silicon_block";
+        public static final String SOLID_METEORIC_IRON_BLOCK = "solid_meteoric_iron_block";
+        public static final String DESH_BLOCK = "desh_block";
+        public static final String TITANIUM_BLOCK = "titanium_block";
+        public static final String LEAD_BLOCK = "lead_block";
+
+        // Decorative BLocks
+        public static final String TIN_DECORATION_BLOCK = "tin_decoration_block";
+        public static final String DARK_DECORATION_BLOCK = "dark_decoration_block";
+        public static final String GRATING_BLOCK = "grating_block";
+        public static final String LIGHTING_PANEL_BLOCK = "lighting_panel_block";
+        public static final String VACUUM_GLASS_BLOCK = "vacuum_glass_block";
+        public static final String VACUUM_GLASS_BLOCK_CLEAR = "vacuum_glass_block_clear_block";
+        public static final String VACUUM_GLASS_BLOCK_TIN = "vacuum_glass_block_tin_block";
+        public static final String VACUUM_GLASS_BLOCK_STRONG = "vacuum_glass_block_strong_block";
+        public static final String WALKWAY_BLOCK = "walkway_block";
+        public static final String MARS_COBBLESTONE_SLAB_BLOCK = "mars_cobblestone_slab";
+        public static final String MARS_DUNGEON_BRICK_SLAB_BLOCK = "mars_dungeon_brick_slab";
+        public static final String MOON_DUNGEON_BRICK_SLAB_BLOCK = "moon_dungeon_brick_slab";
+        public static final String MOON_STONE_SLAB_BLOCK = "moon_stone_slab";
+        public static final String MOON_STONE_STAIRS = "moon_stone_stairs";
+        public static final String MOON_DUNGEON_BRICK_STAIRS = "moon_dungeon_brick_stairs";
+        public static final String TIN_DECORATION_STAIRS = "tin_decoration_stairs";
+        public static final String MARS_DUNGEON_BRICK_STAIRS = "mars_dungeon_brick_stairs";
+        public static final String MARS_COBBLESTONE_STAIRS = "mars_cobblestone_stairs";
+        public static final String TIN_DECORATION_WALL = "tin_decoration_wall";
+        public static final String MOON_STONE_WALL = "moon_stone_wall";
+        public static final String MOON_DUNGEON_BRICK_WALL = "moon_dungeon_brick_wall";
+        public static final String MARS_COBBLESTONE_WALL = "mars_cobblestone_wall";
+        public static final String MARS_DUNGEON_BRICK_WALL = "mars_dungeon_brick_wall";
+
+        // Environment
+        public static final String UNLIT_TORCH = "unlit_torch";
+        public static final String GLOWSTONE_TORCH = "glowstone_torch";
+        public static final String CAVERNOURS_VINE_BLOCK = "cavernous_vine_block";
+        public static final String WEB_TORCH = "web_torch";
+        public static final String FALLEN_METEOR = "fallen_meteor_block";
+        public static final String SLIMELING_EGG_BLOCK = "slimeling_egg_block";
+        public static final String CREEPER_EGG_BLOCK = "creeper_egg_block";
+
+        // Special
+        public static final String PARACHEST_BLOCK = "parachest_block";
+        public static final String SPACE_STATION_ARRIVAL_BLOCK = "space_station_arrival_block";
+        public static final String TREASURE_CHEST_TIER_1 = "treasure_chest_tier_1";
+        public static final String TREASURE_CHEST_TIER_2 = "treasure_chest_tier_2";
+        public static final String TREASURE_CHEST_TIER_3 = "treasure_chest_tier_3";
+        public static final String CHEESE_BLOCK = "cheese_block";
+        public static final String CRASHED_PROBE_BLOCK = "crashed_probe_block";
+
+        // Machines
+        public static final String CIRCUIT_FABRICATOR_BLOCK = "circuit_fabricator_block";
+        public static final String COMPRESSOR_BLOCK = "compressor_block";
+        public static final String ELECTRIC_COMPRESSOR_BLOCK = "electric_compressor_block";
+        public static final String ELECTRIC_FURNACE_BLOCK = "electric_furnace_block";
+        public static final String ELECTRIC_ARC_FURNACE_BLOCK = "electric_arc_furnace_block";
+        public static final String OXYGEN_BUBBLE_DISTRIBUTOR_BLOCK = "oxygen_bubble_distributor_block";
+        public static final String OXYGEN_COLLECTOR_BLOCK = "oxygen_collector_block";
+        public static final String OXYGEN_COMPRESSOR_BLOCK = "oxygen_compressor_block";
+        public static final String OXYGEN_DECOMPRESSOR_BLOCK = "oxygen_decompressor_block";
+        public static final String OXYGEN_DETECTOR_BLOCK = "oxygen_detector_block";
+        public static final String OXYGEN_SEALER_BLOCK = "oxygen_sealer_block";
+        public static final String REFINERY_BLOCK = "refinery_block";
+        public static final String TERRAFORMER_BLOCK = "terraformer_block";
+        public static final String DECONSTRUCTOR_BLOCK = "deconstructor_block";
+        public static final String WATER_ELECTROLYZER_BLOCK = "water_electrolyzer_block";
+        public static final String METHANE_SYNTHESIZIER_BLOCK = "methane_synthesizer_block";
+        public static final String GAS_LIQUEFIER_BLOCK ="gas_liquefier_block";
+
+        // Pad Blocks
+        public static final String BUGGY_FUELING_PAD_BLOCK = "buggy_fueling_block";
+        public static final String ROCKET_LAUNCH_PAD_BLOCK = "rocket_launch_pad_block";
+        public static final String FUEL_LOADER_BLOCK = "fuel_loader_block";
+        public static final String CARGO_LOADER_BLOCK = "cargo_loader_block";
+        public static final String CARGO_UNLOADER_BLOCK = "cargo_unloader_block";
+        public static final String LAUNCH_CONTROLLER_BLOCK = "launch_controller_block";
+
+        // Space Base
+        public static final String HYDRAULIC_PLATFORM_BLOCK = "hydraulic_platform_block";
+        public static final String MAGNETIC_CRAFTING_TABLE_BLOCK = "magnetic_crafting_table_block";
+        public static final String NASA_WORKBENCH_BLOCK = "nasa_workbench_block";
+        public static final String AIR_LOCK_FRAME_BLOCK = "air_lock_frame_block";
+        public static final String AIR_LOCK_CONTROLLER_BLOCK = "air_lock_controller_Block";
+        public static final String AIR_LOCK_SEAL_BLOCK = "air_lock_seal_block";
+        public static final String CHROMATIC_APPLICATOR_BLOCK = "chromatic_applicator_block";
+        public static final String DISPLAY_SCREEN_BLOCK = "display_screen_block";
+        public static final String TELEMETRY_UNIT_BLOCK = "telemetry_unit_block";
+        public static final String COMMUNICATIONS_DISH_BLOCK = "communications_dish_block";
+        public static final String ARC_LAMP_BLOCK = "arc_lamp_block";
+        public static final String SPIN_THRUSTER_BLOCK = "spin_thruster_block";
+        public static final String CRYOGENIC_CHAMBER_BLOCK = "cryogenic_chamber_block";
+        public static final String ASTRO_MINER_BASE_BLOCK = "astro_miner_base_block";
+        public static final String SHORT_RANGE_TELEPAD_BLOCK = "short_range_telepad_block";
+
+        // Power
+        public static final String BASIC_SOLAR_PANEL_BLOCK = "basic_solar_panel_block";
+        public static final String ADVANCED_SOLAR_PANEL_BLOCK = "advanced_solar_panel_block";
+        public static final String COAL_GENERATOR_BLOCK = "coal_generator_block";
+        public static final String GEOTHERMAL_GENERATOR_BLOCK = "geothermal_generator_block";
+        public static final String ENERGY_STORAGE_MODULE_BLOCK = "energy_storage_module_block";
+        public static final String ENERGY_STORAGE_CLUSTER_BLOCK = "energy_storage_cluster_block";
+        public static final String ALUMINUM_WIRE_BLOCK = "aluminum_wire_block";
+        public static final String HEAVY_ALUMINUM_WIRE_BLOCK = "heavy_aluminum_wire_block";
+        public static final String SWITCHABLE_ALUMINUM_WIRE_BLOCK = "switchable_aluminum_wire_block";
+        public static final String SEALABLE_ALUMINUM_WIRE_BLOCK = "sealable_aluminum_wire_block";
+        public static final String HEAVY_SEALABLE_ALUMINUM_WIRE_BLOCK = "heavy_sealable_aluminum_wire_block";
+        public static final String BEAM_REFLECTOR_BLOCK = "beam_reflector_block";
+        public static final String BEAM_RECEIVER_BLOCK = "beam_receiver_block";
+        public static final String SOLAR_ARRAY_MODULE_BLOCK = "solar_array_module_block";
+    }
+
+    // Fluids
+    public static class Fluids {
+        public static final String CRUDE_OIL_FLUID = "crude_oil_fluid";
+        public static final String FUEL_FLUID = "fuel_fluid";
+        public static final String BACTERIAL_ACID_FLUID = "bacterial_acid_fluid";
+        public static final String SULFURIC_ACID_FLUID = "sulfuric_acid_fluid";
     }
 
     //Items
