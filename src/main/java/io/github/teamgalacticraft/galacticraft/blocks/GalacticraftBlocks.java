80b909b4d9dfa9a10c6c259022abc06dbf322c40
Joe van der Zwet <me@joezwet.me>
2019-03-19 20:55:25 +1300

    Change how variants are registered.
    Signed-off-by: Joe van der Zwet <me@joezwet.me>

--- src/main/java/io/github/teamgalacticraft/galacticraft/blocks/GalacticraftBlocks.java
+++ src/main/java/io/github/teamgalacticraft/galacticraft/blocks/GalacticraftBlocks.java
@@ -35,8 +35,14 @@ public class GalacticraftBlocks {
     public static final Block MARS_COBBLESTONE_BLOCK = new Block(FabricBlockSettings.of(Material.STONE, MaterialColor.RED).hardness(2.8F).build());
     public static final Block MARS_DUNGEON_BRICK_BLOCK = new Block(FabricBlockSettings.of(Material.STONE, MaterialColor.GREEN).strength(4.0F, 40.0F).build());
     public static final Block DENSE_ICE_BLOCK = new Block(FabricBlockSettings.of(Material.ICE, MaterialColor.ICE).friction(0.90F).sounds(BlockSoundGroup.GLASS).build());
-    public static final Block ASTEROID_ROCK_BLOCK = new AsteroidRockBlock(FabricBlockSettings.of(Material.STONE, MaterialColor.BROWN).hardness(3.0F).build());
+    public static final Block ASTEROID_ROCK_BLOCK = new Block(FabricBlockSettings.of(Material.STONE, MaterialColor.BROWN).hardness(3.0F).build());
+    public static final Block ASTEROID_ROCK_BLOCK_1 = new Block(FabricBlockSettings.of(Material.STONE, MaterialColor.BROWN).hardness(3.0F).build());
+    public static final Block ASTEROID_ROCK_BLOCK_2 = new Block(FabricBlockSettings.of(Material.STONE, MaterialColor.BROWN).hardness(3.0F).build());
     public static final Block VENUS_ROCK_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6.0F).build());
+    public static final Block VENUS_ROCK_BLOCK_1 = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6.0F).build());
+    public static final Block VENUS_ROCK_BLOCK_2 = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6.0F).build());
+    public static final Block VENUS_ROCK_BLOCK_3 = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6.0F).build());
+    public static final Block VENUS_ROCK_BLOCK_SCORCHED = new ScorcherdRockBlock(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6.0F).build());
     public static final Block VOLCANIC_ROCK_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).build().strength(2.2F, 0.5F));
     public static final Block SCORCHED_ROCK_BLOCK = new ScorcherdRockBlock(FabricBlockSettings.of(Material.STONE).ticksRandomly().build());
     public static final Block PUMICE_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).resistance(1.0F).build());
@@ -65,7 +71,13 @@ public class GalacticraftBlocks {
     public static final BlockItem MARS_DUNGEON_BRICK_BLOCK_ITEM = new BlockItem(MARS_DUNGEON_BRICK_BLOCK, new Item.Settings());
     public static final BlockItem DENSE_ICE_BLOCK_ITEM = new BlockItem(DENSE_ICE_BLOCK, new Item.Settings());
     public static final BlockItem ASTEROID_ROCK_BLOCK_ITEM = new BlockItem(ASTEROID_ROCK_BLOCK, new Item.Settings());
+    public static final BlockItem ASTEROID_ROCK_BLOCK_ITEM_1 = new BlockItem(ASTEROID_ROCK_BLOCK_1, new Item.Settings());
+    public static final BlockItem ASTEROID_ROCK_BLOCK_ITEM_2 = new BlockItem(ASTEROID_ROCK_BLOCK_2, new Item.Settings());
     public static final BlockItem VENUS_ROCK_BLOCK_ITEM = new BlockItem(VENUS_ROCK_BLOCK, new Item.Settings());
+    public static final BlockItem VENUS_ROCK_BLOCK_ITEM_1 = new BlockItem(VENUS_ROCK_BLOCK_1, new Item.Settings());
+    public static final BlockItem VENUS_ROCK_BLOCK_ITEM_2 = new BlockItem(VENUS_ROCK_BLOCK_2, new Item.Settings());
+    public static final BlockItem VENUS_ROCK_BLOCK_ITEM_3 = new BlockItem(VENUS_ROCK_BLOCK_3, new Item.Settings());
+    public static final BlockItem VENUS_ROCK_BLOCK_ITEM_SCORCHED = new BlockItem(VENUS_ROCK_BLOCK_SCORCHED, new Item.Settings());
     public static final BlockItem VOLCANIC_ROCK_BLOCK_ITEM = new BlockItem(VOLCANIC_ROCK_BLOCK, new Item.Settings());
     public static final BlockItem SCORCHED_ROCK_BLOCK_ITEM = new BlockItem(SCORCHED_ROCK_BLOCK, new Item.Settings());
     public static final BlockItem PUMICE_BLOCK_ITEM = new BlockItem(PUMICE_BLOCK, new Item.Settings());
@@ -85,7 +97,7 @@ public class GalacticraftBlocks {
 
     public static ItemGroup BLOCKS_GROUP = FabricItemGroupBuilder.create(
             new Identifier(Constants.MOD_ID, Constants.Blocks.ITEM_GROUP))
-            .stacksForDisplay(itemStack -> {
+            .appendItems(itemStack -> {
                 // add blocks to Creative tab
                 itemStack.add(new ItemStack(MOON_TURF_BLOCK_ITEM));
                 itemStack.add(new ItemStack(MOON_ROCK_BLOCK_ITEM));
@@ -98,7 +110,13 @@ public class GalacticraftBlocks {
                 itemStack.add(new ItemStack(MARS_DUNGEON_BRICK_BLOCK_ITEM));
                 itemStack.add(new ItemStack(DENSE_ICE_BLOCK_ITEM));
                 itemStack.add(new ItemStack(ASTEROID_ROCK_BLOCK_ITEM));
+                itemStack.add(new ItemStack(ASTEROID_ROCK_BLOCK_ITEM_1));
+                itemStack.add(new ItemStack(ASTEROID_ROCK_BLOCK_ITEM_2));
                 itemStack.add(new ItemStack(VENUS_ROCK_BLOCK_ITEM));
+                itemStack.add(new ItemStack(VENUS_ROCK_BLOCK_ITEM_1));
+                itemStack.add(new ItemStack(VENUS_ROCK_BLOCK_ITEM_2));
+                itemStack.add(new ItemStack(VENUS_ROCK_BLOCK_ITEM_3));
+                itemStack.add(new ItemStack(VENUS_ROCK_BLOCK_ITEM_SCORCHED));
                 itemStack.add(new ItemStack(VOLCANIC_ROCK_BLOCK_ITEM));
                 itemStack.add(new ItemStack(SCORCHED_ROCK_BLOCK_ITEM));
                 itemStack.add(new ItemStack(PUMICE_BLOCK_ITEM));
@@ -132,13 +150,19 @@ public class GalacticraftBlocks {
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.MARS_DUNGEON_BRICK_BLOCK), MARS_DUNGEON_BRICK_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.DENSE_ICE_BLOCK), DENSE_ICE_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.ASTEROID_ROCK_BLOCK), ASTEROID_ROCK_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.ASTEROID_ROCK_BLOCK_1), ASTEROID_ROCK_BLOCK_1);
+        Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.ASTEROID_ROCK_BLOCK_2), ASTEROID_ROCK_BLOCK_2);
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.VENUS_ROCK_BLOCK), VENUS_ROCK_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.VENUS_ROCK_BLOCK_1), VENUS_ROCK_BLOCK_1);
+        Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.VENUS_ROCK_BLOCK_2), VENUS_ROCK_BLOCK_2);
+        Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.VENUS_ROCK_BLOCK_3), VENUS_ROCK_BLOCK_3);
+        Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.VENUS_ROCK_BLOCK_SCORCHED), VENUS_ROCK_BLOCK_SCORCHED);
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.VOLCANIC_ROCK_BLOCK), VOLCANIC_ROCK_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.SCORCHED_ROCK_BLOCK), SCORCHED_ROCK_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.PUMICE_BLOCK), PUMICE_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.VAPOR_SPOUT_BLOCK), VAPOR_SPOUT_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.TIN_DECORATION_BLOCK), TIN_DECORATION_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.TIN_WALL_BLOCK), TIN_WALL_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.TIN_DECORATION_WALL), TIN_WALL_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.COPPER_ORE_BLOCK), COPPER_ORE_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.TIN_ORE_BLOCK), TIN_ORE_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.ALUMINUM_ORE_BLOCK), ALUMINUM_ORE_BLOCK);
@@ -149,16 +173,6 @@ public class GalacticraftBlocks {
         Registry.register(Registry.BLOCK, new Identifier(Constants.MOD_ID, Constants.Blocks.SILICON_BLOCK), SILICON_BLOCK);
 
         // Register Block items
-        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.TIN_DECORATION_BLOCK), TIN_DECORATION_BLOCK_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.TIN_WALL_BLOCK), TIN_WALL_BLOCK_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.COPPER_ORE_BLOCK), COPPER_ORE_BLOCK_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.TIN_ORE_BLOCK), TIN_ORE_BLOCK_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.ALUMINUM_ORE_BLOCK), ALUMINUM_ORE_BLOCK_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.SILICON_ORE_BLOCk), SILICON_ORE_BLOCK_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.COPPER_BLOCK), COPPER_BLOCK_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.TIN_BLOCK), TIN_BLOCK_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.ALUMINUM_BLOCK), ALUMINUM_BLOCK_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.SILICON_BLOCK), SILICON_BLOCK_ITEM);
         Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.MOON_TURF), MOON_TURF_BLOCK_ITEM);
         Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.MOON_ROCK), MOON_ROCK_BLOCK_ITEM);
         Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.MOON_DIRT), MOON_DIRT_BLOCK_ITEM);
@@ -170,10 +184,26 @@ public class GalacticraftBlocks {
         Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.MARS_DUNGEON_BRICK_BLOCK), MARS_DUNGEON_BRICK_BLOCK_ITEM);
         Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.DENSE_ICE_BLOCK), DENSE_ICE_BLOCK_ITEM);
         Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.ASTEROID_ROCK_BLOCK), ASTEROID_ROCK_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.ASTEROID_ROCK_BLOCK_1), ASTEROID_ROCK_BLOCK_ITEM_1);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.ASTEROID_ROCK_BLOCK_2), ASTEROID_ROCK_BLOCK_ITEM_2);
         Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.VENUS_ROCK_BLOCK), VENUS_ROCK_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.VENUS_ROCK_BLOCK_1), VENUS_ROCK_BLOCK_ITEM_1);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.VENUS_ROCK_BLOCK_2), VENUS_ROCK_BLOCK_ITEM_2);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.VENUS_ROCK_BLOCK_3), VENUS_ROCK_BLOCK_ITEM_3);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.VENUS_ROCK_BLOCK_SCORCHED), VENUS_ROCK_BLOCK_ITEM_SCORCHED);
         Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.VOLCANIC_ROCK_BLOCK), VOLCANIC_ROCK_BLOCK_ITEM);
         Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.SCORCHED_ROCK_BLOCK), SCORCHED_ROCK_BLOCK_ITEM);
         Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.PUMICE_BLOCK), PUMICE_BLOCK_ITEM);
         Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.VAPOR_SPOUT_BLOCK), VAPOR_SPOUT_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.TIN_DECORATION_BLOCK), TIN_DECORATION_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.TIN_DECORATION_WALL), TIN_WALL_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.COPPER_ORE_BLOCK), COPPER_ORE_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.TIN_ORE_BLOCK), TIN_ORE_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.ALUMINUM_ORE_BLOCK), ALUMINUM_ORE_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.SILICON_ORE_BLOCk), SILICON_ORE_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.COPPER_BLOCK), COPPER_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.TIN_BLOCK), TIN_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.ALUMINUM_BLOCK), ALUMINUM_BLOCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(Constants.MOD_ID, Constants.Blocks.SILICON_BLOCK), SILICON_BLOCK_ITEM);
     }
 }
