869c5086c2d6518e8e733ab28c092942144fac4d
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2019-03-25 13:40:29 -0700

    Tags!
    (More things to copy & paste! yay)
    More will come, I just need to get this class up onto github

---
+++ src/main/java/io/github/teamgalacticraft/galacticraft/tag/GCItemTags.java
@@ -0,0 +1,13 @@
+package io.github.teamgalacticraft.galacticraft.tag;
+
+import io.github.teamgalacticraft.galacticraft.Constants;
+import net.minecraft.item.Item;
+import net.minecraft.tag.ItemTags;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
+
+public class GCItemTags {
+    public static final Tag<Item> DESH_INGOT = new ItemTags.class_3490(new Identifier(Constants.MOD_ID, Constants.Items.DESH_INGOT));
+    public static final Tag<Item> TITANIUM_INGOT = new ItemTags.class_3490(new Identifier(Constants.MOD_ID, Constants.Items.TITANIUM_INGOT));
+
+}
