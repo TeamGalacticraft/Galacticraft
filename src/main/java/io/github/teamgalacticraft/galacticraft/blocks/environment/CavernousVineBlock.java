e9da425e063a07458f20695c6b68b8d62af9a3d0
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2019-03-26 16:25:08 -0700

    Keep x + z velocity

--- src/main/java/io/github/teamgalacticraft/galacticraft/blocks/environment/CavernousVineBlock.java
+++ src/main/java/io/github/teamgalacticraft/galacticraft/blocks/environment/CavernousVineBlock.java
@@ -26,7 +26,7 @@ public class CavernousVineBlock extends Block implements Waterloggable {
     public void onEntityCollision(BlockState blockState_1, World world_1, BlockPos blockPos_1, Entity entity_1) {
         super.onEntityCollision(blockState_1, world_1, blockPos_1, entity_1);
         entity_1.damage(GCDamageSource.VINE_POISON, 5.0f);
-        entity_1.setVelocity(0.0D, 0.15D, 0.0D);
+        entity_1.setVelocity(entity_1.getVelocity().x, 0.15D, entity_1.getVelocity().z);
     }
 
     @Override
