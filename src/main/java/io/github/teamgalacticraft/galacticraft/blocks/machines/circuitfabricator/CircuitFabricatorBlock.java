1b8c4572c9d4c360566dd8fa1ba950fdf72bf6b8
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2019-03-31 18:27:25 -0700

    Get rid of duplicate CircuitFabricatorBlock

--- src/main/java/io/github/teamgalacticraft/galacticraft/blocks/machines/circuitfabricator/CircuitFabricatorBlock.java
+++ src/main/java/io/github/teamgalacticraft/galacticraft/blocks/machines/circuitfabricator/CircuitFabricatorBlock.java
@@ -1,7 +1,16 @@
-package io.github.teamgalacticraft.galacticraft.blocks.machines.circuitfabricator;
+package io.github.teamgalacticraft.galacticraft.blocks.machines;
+
+import io.github.teamgalacticraft.galacticraft.api.blocks.AbstractHorizontalDirectionalBlock;
+import io.github.teamgalacticraft.galacticraft.util.Rotatable;
 
 /**
  * @author <a href="https://github.com/teamgalacticraft">TeamGalacticraft</a>
  */
-public class CircuitFabricatorBlock {
+public class CircuitFabricatorBlock extends AbstractHorizontalDirectionalBlock implements Rotatable {
+
+
+    public CircuitFabricatorBlock(Settings settings) {
+        super(settings);
+    }
+
 }
