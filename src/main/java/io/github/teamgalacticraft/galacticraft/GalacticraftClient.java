180fd04eb52d200c700d2ae4d85cb78629a81068
Blake Redenius <blakeredenius@gmail.com>
2019-04-08 22:55:18 +1000

    Fix run server always failing.

--- src/main/java/io/github/teamgalacticraft/galacticraft/GalacticraftClient.java
+++ src/main/java/io/github/teamgalacticraft/galacticraft/GalacticraftClient.java
@@ -1,17 +1,21 @@
 package io.github.teamgalacticraft.galacticraft;
 
 import io.github.teamgalacticraft.galacticraft.blocks.machines.coalgenerator.CoalGeneratorScreen;
+import io.github.teamgalacticraft.galacticraft.config.ConfigHandler;
 import io.github.teamgalacticraft.galacticraft.container.GalacticraftContainers;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screen.ScreenProviderRegistry;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * @author <a href="https://github.com/teamgalacticraft">TeamGalacticraft</a>
  */
 public class GalacticraftClient implements ClientModInitializer {
-
 
     @Override
     public void onInitializeClient() {
@@ -21,5 +25,16 @@ public class GalacticraftClient implements ClientModInitializer {
 
         ScreenProviderRegistry.INSTANCE.registerFactory(GalacticraftContainers.COAL_GENERATOR_CONTAINER, (syncId, identifier, playerEntity, packetByteBuf) -> new CoalGeneratorScreen(syncId, packetByteBuf.readBlockPos(), playerEntity));
         //ScreenProviderRegistry.INSTANCE.registerFactory(GalacticraftContainers.PLAYER_INVENTORY_CONTAINER, (syncId, identifier, playerEntity, packetByteBuf) -> new PlayerInventoryScreen());
+
+        if (FabricLoader.getInstance().isModLoaded("modmenu")) {
+            try {
+                Class<?> clazz = Class.forName("io.github.prospector.modmenu.api.ModMenuApi");
+                Method method = clazz.getMethod("addConfigOverride", String.class, Runnable.class);
+                method.invoke(null, Constants.MOD_ID, (Runnable) () -> Galacticraft.configHandler.openConfigScreen());
+            } catch (NoSuchMethodException | ClassNotFoundException | IllegalAccessException | InvocationTargetException e) {
+                Galacticraft.logger.error("[Galacticraft] Failed to add modmenu config override. {1}", e);
+            }
+        }
+
     }
 }
