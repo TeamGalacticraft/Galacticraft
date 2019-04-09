f049bf4120570398f37f0cb97d9f67f4bca7b1dd
Blake Redenius <blakeredenius@gmail.com>
2019-04-09 10:40:10 +1000

    Fix / fully implement waterlogging on Grating Blocks.

--- src/main/java/io/github/teamgalacticraft/galacticraft/blocks/decoration/GratingBlock.java
+++ src/main/java/io/github/teamgalacticraft/galacticraft/blocks/decoration/GratingBlock.java
@@ -4,12 +4,20 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Waterloggable;
 import net.minecraft.entity.VerticalEntityPosition;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateFactory;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.IWorld;
 
 public class GratingBlock extends Block implements Waterloggable {
-
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
     public GratingBlock(Settings settings) {
         super(settings);
@@ -17,7 +25,36 @@ public class GratingBlock extends Block implements Waterloggable {
 
     @Override
     public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPos, VerticalEntityPosition verticalEntityPosition) {
-
         return Block.createCuboidShape(0.0D, 14.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    }
+
+    @Override
+    public boolean isTranslucent(BlockState blockState, BlockView blockView, BlockPos blockPos) {
+        return !blockState.get(WATERLOGGED);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        FluidState fluidState_1 = context.getWorld().getFluidState(context.getBlockPos());
+        return super.getPlacementState(context).with(WATERLOGGED, fluidState_1.getFluid() == Fluids.WATER);
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState blockState, Direction direction, BlockState neighborBlockState, IWorld world, BlockPos blockPos, BlockPos neighborBlockPos) {
+        if (blockState.get(WATERLOGGED)) {
+            world.getFluidTickScheduler().schedule(blockPos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+
+        return super.getStateForNeighborUpdate(blockState, direction, neighborBlockState, world, blockPos, neighborBlockPos);
+    }
+
+    @Override
+    protected void appendProperties(StateFactory.Builder<Block, BlockState> builder) {
+        builder.with(WATERLOGGED);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState blockState) {
+        return blockState.get(WATERLOGGED) ? Fluids.WATER.getState(false) : super.getFluidState(blockState);
     }
 }
