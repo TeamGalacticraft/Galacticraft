3236fff36de4b5b5154d95611a3bd1a526fc97ec
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2019-05-10 15:47:24 -0700

    Fix wire model

--- src/main/java/com/hrznstudio/galacticraft/blocks/ore/SiliconOreBlock.java
+++ src/main/java/com/hrznstudio/galacticraft/blocks/ore/SiliconOreBlock.java
@@ -1,11 +1,6 @@
 package com.hrznstudio.galacticraft.blocks.ore;
 
-import com.hrznstudio.galacticraft.blocks.GalacticraftBlocks;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.OreBlock;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.BlockView;
 
 /**
  * @author <a href="https://github.com/StellarHorizons">StellarHorizons</a>
@@ -16,8 +11,4 @@ public class SiliconOreBlock extends OreBlock {
         super(settings);
     }
 
-    @Override
-    public ItemStack getPickStack(BlockView blockView, BlockPos pos, BlockState state) {
-        return new ItemStack(GalacticraftBlocks.SILICON_ORE_ITEM);
-    }
 }
