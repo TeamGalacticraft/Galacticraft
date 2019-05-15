4452d1289b8e84993376bee9308bb7a8d70e65ca
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2019-05-15 16:56:47 -0700

    More RP stuff
    Still doesn't work on normal clients

--- src/main/java/com/hrznstudio/galacticraft/mixin/ClientResourcePackCreatorMixin.java
+++ src/main/java/com/hrznstudio/galacticraft/mixin/ClientResourcePackCreatorMixin.java
@@ -10,11 +10,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.CopyOption;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
+import java.io.*;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
 @Mixin(ClientResourcePackCreator.class)
@@ -26,19 +26,45 @@ public class ClientResourcePackCreatorMixin {
     private void registerContainer(Map<String, ResourcePackContainer> map, ResourcePackContainer.Factory<?> packContainerFactory, CallbackInfo ci) {
         if (!dl) {
             dl = true;
-            if (!new File (MinecraftClient.getInstance().runDirectory.toString() + "/assets/objects/B7/B7FEE1C45FAB62449D4B96C74F13BDBAB05A30B1".toLowerCase()).exists()) {
-                if (!new File (MinecraftClient.getInstance().runDirectory.toString() + "/assets/objects/B7/".toLowerCase()).isDirectory()) {
-                    new File(MinecraftClient.getInstance().runDirectory.toString() + "/assets/objects/B7".toLowerCase()).mkdirs();
+            if (!new File (MinecraftClient.getInstance().runDirectory.toString() + "/assets/objects/b7/b7fee1c45fab62449d4b96c74f13bdbab05a30b1").exists()) {
+                if (!new File(MinecraftClient.getInstance().runDirectory.toString() + "/assets/objects/b7/").isDirectory()) {
+                    new File(MinecraftClient.getInstance().runDirectory.toString() + "/assets/objects/b7").mkdirs();
                 }
                 try {
-                    Files.copy(new File((Galacticraft.class.getResource("GalacticraftClient.class").getFile().replace("com/hrznstudio/galacticraft/GalacticraftClient.class", "") + "B7FEE1C45FAB62449D4B96C74F13BDBAB05A30B1").replace("%20", " ").substring(1)).toPath(), new File (MinecraftClient.getInstance().runDirectory.toString() + "/assets/objects/B7/B7FEE1C45FAB62449D4B96C74F13BDBAB05A30B1".toLowerCase()).toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    InputStream in = new BufferedInputStream(new FileInputStream(
+                            new File((Galacticraft.class.getResource("GalacticraftClient.class").getFile().replace("com/hrznstudio/galacticraft/GalacticraftClient.class", "") + "b7fee1c45fab62449d4b96c74f13bdbab05a30b1").replace("%20", " "))));
+                    OutputStream out = new BufferedOutputStream(new FileOutputStream(
+                            new File((MinecraftClient.getInstance().runDirectory.toString() + "/assets/objects/b7/b7fee1c45fab62449d4b96c74f13bdbab05a30b1").replace("file:\\", ""))));
+
+                    byte[] buffer = new byte[1024];
+                    int lengthRead;
+                    while ((lengthRead = in.read(buffer)) > 0) {
+                        out.write(buffer, 0, lengthRead);
+                        out.flush();
+                    }
                 } catch (IOException e) {
                     Galacticraft.logger.error("Unable to copy the new GC Textures!");
                     e.printStackTrace();
                 }
+            } else {
+                try {
+                    MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+                    try (InputStream input = new FileInputStream(new File (MinecraftClient.getInstance().runDirectory.toString() + "/assets/objects/b7/b7fee1c45fab62449d4b96c74f13bdbab05a30b1"))) {
+                        byte[] buffer = new byte[8192];
+                        int len = input.read(buffer);
+                        while (len != -1) {
+                            sha1.update(buffer, 0, len);
+                            len = input.read(buffer);
+                        }
+
+                        new HexBinaryAdapter().marshal(sha1.digest());
+                    }
+                } catch (IOException | NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
             }
         }
-        File resourcePackFile = new File (MinecraftClient.getInstance().runDirectory.toString() + "/assets/objects/B7/B7FEE1C45FAB62449D4B96C74F13BDBAB05A30B1".toLowerCase());
+        File resourcePackFile = new File (MinecraftClient.getInstance().runDirectory.toString() + "/assets/objects/b7/b7fee1c45fab62449d4b96c74f13bdbab05a30b1");
         if (resourcePackFile.isFile()) {
             ResourcePackContainer packContainer = ResourcePackContainer.of("galacticraft-rewoven:new_textures", false, () -> new NewGalacticraftTexturePack(resourcePackFile), packContainerFactory, ResourcePackContainer.SortingDirection.TOP);
             if (packContainer != null) {
