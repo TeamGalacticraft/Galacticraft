87c00bd18a5a209714a105a0cb7c8cc912c27e2d
Joe van der Zwet <me@joezwet.me>
2019-05-25 14:04:58 +1200

    Add hyphens back to uuid

--- tools/capeTxt2Json.py
+++ tools/capeTxt2Json.py
@@ -13,6 +13,7 @@ for line in capesTxt:
     name = line.split(':')[1].split(' ')[1]
 
     name = name[0:len(name)-1] # remove \n at end of name
+    uuid = uuid[0:8] + '-' + uuid[8:12] + '-' + uuid[12:16] + '-' + uuid[16:20] + '-' + uuid[20:32] # add hyphens back to uuid string
 
     player = {}
     player['uuid'] = uuid
