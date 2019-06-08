490ebcd0749ccd4a42a8d93ec87fafbf9562f236
Chip Wolf ‮ <hello@chipwolf.uk>
2019-06-08 23:53:54 +0100

    :zap: attempting new build strategy

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+export TERM=${TERM:-dumb}
 export BUILD_NUMBER=$(cat ../version/number)
-./gradlew build publish --no-daemon
+export ROOT_FOLDER=$(pwd)
+export GRADLE_USER_HOME="${ROOT_FOLDER}/.gradle"
+
+./gradlew --scan --stacktrace --debug clean build publish
