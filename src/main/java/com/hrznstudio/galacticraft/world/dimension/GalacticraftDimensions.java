a53365eb43292a5b9f2a353632f1e809d2bed38e
Joe van der Zwet <me@joezwet.me>
2019-09-22 15:43:09 +1200

    :alien: Switch to Fabric Dim API

--- src/main/java/com/hrznstudio/galacticraft/world/dimension/GalacticraftDimensions.java
+++ src/main/java/com/hrznstudio/galacticraft/world/dimension/GalacticraftDimensions.java
@@ -22,17 +22,17 @@
 
 package com.hrznstudio.galacticraft.world.dimension;
 
-import net.minecraft.util.registry.Registry;
+import com.hrznstudio.galacticraft.Constants;
+import net.fabricmc.fabric.api.dimension.v1.FabricDimensionType;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.dimension.DimensionType;
 
 /**
  * @author <a href="https://github.com/StellarHorizons">StellarHorizons</a>
  */
 public class GalacticraftDimensions {
-    public static final DimensionType MOON = Registry.register(Registry.DIMENSION, 30, "galacticraft-rewoven:moon", new GalacticraftDimensionType(30, "galacticraft-rewoven:moon", "DIM30", MoonDimension::new, true));
-    public static final DimensionType MARS = Registry.register(Registry.DIMENSION, 31, "galacticraft-rewoven:mars", new GalacticraftDimensionType(31, "galacticraft-rewoven:mars", "DIM31", MarsDimension::new, true));
+    public static final DimensionType MOON = FabricDimensionType.builder().skyLight(true).desiredRawId(30).factory(MoonDimension::new).buildAndRegister(new Identifier(Constants.MOD_ID, "moon"));
+    public static final DimensionType MARS = FabricDimensionType.builder().skyLight(true).desiredRawId(31).factory(MarsDimension::new).buildAndRegister(new Identifier(Constants.MOD_ID, "mars"));
 
-
-    public static void init() {
-    }
+    public static void init() {}
 }
