98b8f4c00b04f8f0dcce27e7ba9b67b57464e813
Joe van der Zwet <me@joezwet.me>
2020-02-20 10:56:44 +1300

    :wrench::fire: Remove need for Silk API. Update fabric.mod.json.

---
+++ src/main/java/com/hrznstudio/galacticraft/api/block/StairsBlock.java
@@ -0,0 +1,9 @@
+package com.hrznstudio.galacticraft.api.block;
+
+import net.minecraft.block.BlockState;
+
+public class StairsBlock extends net.minecraft.block.StairsBlock {
+    public StairsBlock(BlockState baseBlockState, Settings settings) {
+        super(baseBlockState, settings);
+    }
+}
---
+++ src/main/java/com/hrznstudio/galacticraft/api/item/AxeItem.java
@@ -0,0 +1,9 @@
+package com.hrznstudio.galacticraft.api.item;
+
+import net.minecraft.item.ToolMaterial;
+
+public class AxeItem extends net.minecraft.item.AxeItem {
+    public AxeItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
