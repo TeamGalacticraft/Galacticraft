525a73e3d7d5c29e1e6fc846b0088f195cf1f672
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2020-04-02 23:00:19 -0700

    :page_facing_up: Prepend license header to files missing it

--- src/main/java/com/hrznstudio/galacticraft/client/render/entity/moonvillager/MoonVillagerRenderer.java
+++ src/main/java/com/hrznstudio/galacticraft/client/render/entity/moonvillager/MoonVillagerRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 HRZN LTD
+ * Copyright (c) 2019 HRZN LTD
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
