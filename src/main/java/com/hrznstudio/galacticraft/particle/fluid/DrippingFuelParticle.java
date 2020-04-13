f2f3f2a7d1d9dd08b90702c7262aa9bae5f05e80
Brendan Lewis <brendanelewis@outlook.com>
2020-04-12 21:18:42 -0400

    :sparkles: This might be particles!

--- src/main/java/com/hrznstudio/galacticraft/particle/fluid/DrippingFuelParticle.java
+++ src/main/java/com/hrznstudio/galacticraft/particle/fluid/DrippingFuelParticle.java
@@ -42,13 +42,13 @@ public class DrippingFuelParticle extends SpriteBillboardParticle {
         setSprite(MinecraftClient.getInstance().getItemRenderer().getModels().getSprite(Blocks.ACACIA_LOG.asItem()));
         this.scale *= 0.25f;
         this.velocityX = 0.0f;
-        this.velocityY = 0.0f;
-        this.velocityZ = -0.01f;
+        this.velocityY = -0.98f;
+        this.velocityZ = 0.0f;
         this.colorRed = 42f/255f;
         this.colorGreen = 42f/255f;
         this.colorBlue = 42f/255f;
         this.colorAlpha = 229f/255f;
-        this.maxAge = 500;
+        this.maxAge = (int)(64.0D / (Math.random() * 0.8D + 0.2D));
     }
 
     @Override
