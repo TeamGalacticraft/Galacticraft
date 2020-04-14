5c50f5ab2fee0f1c6700d4f1413991ecbccfcb05
Brendan Lewis <brendanelewis@outlook.com>
2020-04-14 18:38:37 -0400

    :construction: More fluid, for walkway specifically

--- src/main/java/com/hrznstudio/galacticraft/blocks/special/walkway/Walkway.java
+++ src/main/java/com/hrznstudio/galacticraft/blocks/special/walkway/Walkway.java
@@ -24,33 +24,58 @@ package com.hrznstudio.galacticraft.blocks.special.walkway;
 
 import com.hrznstudio.galacticraft.blocks.FluidLoggableBlock;
 import com.hrznstudio.galacticraft.blocks.decoration.GratingBlock;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.minecraft.block.*;
+import net.minecraft.entity.EntityContext;
 import net.minecraft.fluid.BaseFluid;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateFactory;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.SystemUtil;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.IWorld;
 
 import javax.annotation.Nullable;
+import java.util.Map;
 
 /**
  * @author <a href="https://github.com/StellarHorizons">StellarHorizons</a>
  */
-public class Walkway extends HorizontalConnectedBlock implements FluidLoggableBlock {
+public class Walkway extends Block implements FluidLoggableBlock {
+
+    public static final BooleanProperty NORTH;
+    public static final BooleanProperty EAST;
+    public static final BooleanProperty SOUTH;
+    public static final BooleanProperty WEST;
+
+    private static final Map<Direction, BooleanProperty> FACING_PROPERTIES;
+    private final VoxelShape[] collisionShapes;
+    private final VoxelShape[] boundingShapes;
+    private final Object2IntMap<BlockState> SHAPE_INDEX_CACHE = new Object2IntOpenHashMap();
 
     public Walkway(Settings settings) {
-        super(8,8,16,16,16f, settings);
+        super(settings);
+
+        this.collisionShapes = this.createShapes(8, 8, 16f, 0.0F, 16f);
+        this.boundingShapes = this.createShapes(8, 8, 16, 0.0F, 16);
+
         this.setDefaultState(this.stateFactory.getDefaultState()
                 .with(NORTH, false)
                 .with(EAST, false)
                 .with(SOUTH, false)
                 .with(WEST, false)
-                .with(WATERLOGGED, false)
                 .with(FLUID, new Identifier("empty"))
                 .with(BaseFluid.LEVEL, 8));
     }
@@ -88,6 +113,104 @@ public class Walkway extends HorizontalConnectedBlock implements FluidLoggableBl
 
     @Override
     public void appendProperties(StateFactory.Builder<Block, BlockState> stateBuilder) {
-        stateBuilder.add(NORTH, EAST, WEST, SOUTH, WATERLOGGED, FLUID, BaseFluid.LEVEL);
+        stateBuilder.add(NORTH, EAST, WEST, SOUTH, FLUID, BaseFluid.LEVEL);
+    }
+
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        switch(rotation) {
+            case CLOCKWISE_180:
+                return state.with(NORTH, state.get(SOUTH)).with(EAST, state.get(WEST)).with(SOUTH, state.get(NORTH)).with(WEST, state.get(EAST));
+            case COUNTERCLOCKWISE_90:
+                return state.with(NORTH, state.get(EAST)).with(EAST, state.get(SOUTH)).with(SOUTH, state.get(WEST)).with(WEST, state.get(NORTH));
+            case CLOCKWISE_90:
+                return state.with(NORTH, state.get(WEST)).with(EAST, state.get(NORTH)).with(SOUTH, state.get(EAST)).with(WEST, state.get(SOUTH));
+            default:
+                return state;
+        }
+    }
+
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        switch(mirror) {
+            case LEFT_RIGHT:
+                return (BlockState)((BlockState)state.with(NORTH, state.get(SOUTH))).with(SOUTH, state.get(NORTH));
+            case FRONT_BACK:
+                return (BlockState)((BlockState)state.with(EAST, state.get(WEST))).with(WEST, state.get(EAST));
+            default:
+                return super.mirror(state, mirror);
+        }
+    }
+
+    public boolean canPlaceAtSide(BlockState world, BlockView view, BlockPos pos, BlockPlacementEnvironment env) {
+        return false;
+    }
+
+    private VoxelShape[] createShapes(float radius1, float radius2, float height1, float offset2, float height2) {
+        float f = 8.0F - radius1;
+        float g = 8.0F + radius1;
+        float h = 8.0F - radius2;
+        float i = 8.0F + radius2;
+        VoxelShape voxelShape = Block.createCuboidShape(f, 0.0D, f, g, height1, g);
+        VoxelShape voxelShape2 = Block.createCuboidShape(h, offset2, 0.0D, i, height2, i);
+        VoxelShape voxelShape3 = Block.createCuboidShape(h, offset2, h, i, height2, 16.0D);
+        VoxelShape voxelShape4 = Block.createCuboidShape(0.0D, offset2, h, i, height2, i);
+        VoxelShape voxelShape5 = Block.createCuboidShape(h, offset2, h, 16.0D, height2, i);
+        VoxelShape voxelShape6 = VoxelShapes.union(voxelShape2, voxelShape5);
+        VoxelShape voxelShape7 = VoxelShapes.union(voxelShape3, voxelShape4);
+        VoxelShape[] voxelShapes = new VoxelShape[]{VoxelShapes.empty(), voxelShape3, voxelShape4, voxelShape7, voxelShape2, VoxelShapes.union(voxelShape3, voxelShape2), VoxelShapes.union(voxelShape4, voxelShape2), VoxelShapes.union(voxelShape7, voxelShape2), voxelShape5, VoxelShapes.union(voxelShape3, voxelShape5), VoxelShapes.union(voxelShape4, voxelShape5), VoxelShapes.union(voxelShape7, voxelShape5), voxelShape6, VoxelShapes.union(voxelShape3, voxelShape6), VoxelShapes.union(voxelShape4, voxelShape6), VoxelShapes.union(voxelShape7, voxelShape6)};
+
+        for(int j = 0; j < 16; ++j) {
+            voxelShapes[j] = VoxelShapes.union(voxelShape, voxelShapes[j]);
+        }
+
+        return voxelShapes;
+    }
+
+    public boolean isTranslucent(BlockState state, BlockView view, BlockPos pos) {
+        return true;
+    }
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, EntityContext ePos) {
+        return this.boundingShapes[this.getShapeIndex(state)];
+    }
+
+    public VoxelShape getCollisionShape(BlockState state, BlockView view, BlockPos pos, EntityContext ePos) {
+        return this.collisionShapes[this.getShapeIndex(state)];
+    }
+
+    private static int getDirectionMask(Direction dir) {
+        return 1 << dir.getHorizontal();
+    }
+
+    protected int getShapeIndex(BlockState state) {
+        return this.SHAPE_INDEX_CACHE.computeIntIfAbsent(state, (blockState) -> {
+            int i = 0;
+            if (blockState.get(NORTH)) {
+                i |= getDirectionMask(Direction.NORTH);
+            }
+
+            if (blockState.get(EAST)) {
+                i |= getDirectionMask(Direction.EAST);
+            }
+
+            if (blockState.get(SOUTH)) {
+                i |= getDirectionMask(Direction.SOUTH);
+            }
+
+            if (blockState.get(WEST)) {
+                i |= getDirectionMask(Direction.WEST);
+            }
+
+            return i;
+        });
+    }
+
+    static {
+        NORTH = ConnectedPlantBlock.NORTH;
+        EAST = ConnectedPlantBlock.EAST;
+        SOUTH = ConnectedPlantBlock.SOUTH;
+        WEST = ConnectedPlantBlock.WEST;
+        FACING_PROPERTIES = ConnectedPlantBlock.FACING_PROPERTIES.entrySet().stream().filter((entry) -> {
+            return ((Direction)entry.getKey()).getAxis().isHorizontal();
+        }).collect(SystemUtil.toMap());
     }
 }
