8bd97bd99594360e3706208973e0bd144a86db52
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2020-04-24 11:13:46 -0700

    :bug: Improve oxygen suffocation mixin
    Signed-off-by: marcus8448 <28928887+marcus8448@users.noreply.github.com>

--- src/main/java/com/hrznstudio/galacticraft/mixin/LivingEntityMixin.java
+++ src/main/java/com/hrznstudio/galacticraft/mixin/LivingEntityMixin.java
@@ -26,65 +26,106 @@ import alexiil.mc.lib.attributes.item.impl.FullFixedItemInv;
 import com.hrznstudio.galacticraft.accessor.GCPlayerAccessor;
 import com.hrznstudio.galacticraft.api.atmosphere.AtmosphericGas;
 import com.hrznstudio.galacticraft.api.celestialbodies.CelestialBodyType;
-import com.hrznstudio.galacticraft.api.entity.EvolvedEntity;
 import com.hrznstudio.galacticraft.entity.damage.GalacticraftDamageSource;
 import com.hrznstudio.galacticraft.items.OxygenTankItem;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectUtil;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.tag.FluidTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * @author <a href="https://github.com/StellarHorizons">StellarHorizons</a>
  */
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin {
-
-    private int air;
-
-    @Inject(method = "baseTick", at = @At("HEAD"))
-    private void baseTick(CallbackInfo ci) {
-        try {
-            if (((PlayerEntity) (Object) this).isCreative()) {
-                air = 0;
-                return;
-            }
-        } catch (ClassCastException ignore) {
-        }
-        air = ((LivingEntity) (Object) this).getAir();
+public abstract class LivingEntityMixin extends Entity {
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
     }
 
-    @Inject(method = "baseTick", at = @At("RETURN"))
-    private void oxygenDamage(CallbackInfo ci) {
-        Entity entity = (LivingEntity) (Object) this;
-        if (entity.isAlive()) {
-            if(CelestialBodyType.getByDimType(entity.world.dimension.getType()).isPresent()) {
-                if (!CelestialBodyType.getByDimType(entity.world.dimension.getType()).get().getAtmosphere().getComposition().containsKey(AtmosphericGas.OXYGEN)) {
-                    entity.setAir(air - 1);
-                    if (entity.getAir() == -20) {
-                        entity.setAir(0);
-                        air = 0;
-                        try {
-                            FullFixedItemInv gearInventory = ((GCPlayerAccessor) entity).getGearInventory();
-                            if (gearInventory.getInvStack(6).getItem() instanceof OxygenTankItem && ((gearInventory.getInvStack(6).getMaxDamage() - gearInventory.getInvStack(6).getDamage()) > 0)) {
-                                gearInventory.getInvStack(6).setDamage(gearInventory.getInvStack(6).getDamage() + 1);
-                                return;
-                            } else if (gearInventory.getInvStack(7).getItem() instanceof OxygenTankItem && ((gearInventory.getInvStack(7).getMaxDamage() - gearInventory.getInvStack(7).getDamage()) > 0)) {
-                                gearInventory.getInvStack(7).setDamage(gearInventory.getInvStack(7).getDamage() + 1);
-                                return;
-                            }
-                        } catch (ClassCastException ignore) {
-                            if (entity instanceof EvolvedEntity) {
-                                return;
-                            }
-                        }
-                        entity.damage(GalacticraftDamageSource.SUFFOCATION, 2.0F);
+    @Shadow
+    public abstract boolean canBreatheInWater();
+
+    @Shadow
+    public abstract boolean isAlive();
+
+    @Shadow
+    protected abstract int getNextAirUnderwater(int air);
+
+    @Redirect(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getNextAirOnLand(I)I"))
+    private int skipAirCheck_1gc(LivingEntity livingEntity, int air) {
+        return air;
+    }
+
+    @Redirect(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getNextAirUnderwater(I)I"))
+    private int skipAirCheck_2gc(LivingEntity livingEntity, int air) {
+        return air;
+    }
+
+    @Redirect(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getAir()I", ordinal = 0))
+    private int skipAirCheck_3gc(LivingEntity livingEntity) {
+        return 0; //not -20
+    }
+
+    @Inject(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;tickStatusEffects()V", shift = At.Shift.BEFORE))
+    private void doOxygenChecks(CallbackInfo ci) {
+        if (this.isAlive()) {
+            if (CelestialBodyType.getByDimType(world.dimension.getType()).isPresent() && !CelestialBodyType.getByDimType(world.dimension.getType()).get().getAtmosphere().getComposition().containsKey(AtmosphericGas.OXYGEN)) {
+                updateAir(this);
+            } else {
+                if (this.isInFluid(FluidTags.WATER) && this.world.getBlockState(new BlockPos(this.getX(), this.getEyeY(), this.getZ())).getBlock() != Blocks.BUBBLE_COLUMN) {
+                    if (!this.canBreatheInWater() && !StatusEffectUtil.hasWaterBreathing((LivingEntity) (Object) this) && !isInvulnerableTo(DamageSource.DROWN)) {
+                        this.setAir(this.getNextAirUnderwater(this.getAir()));
                     }
                 }
             }
+
+            if (this.getAir() == -20) {
+                this.setAir(0);
+
+                if (this.isInFluid(FluidTags.WATER) && this.world.getBlockState(new BlockPos(this.getX(), this.getEyeY(), this.getZ())).getBlock() != Blocks.BUBBLE_COLUMN) {
+                    if (!this.canBreatheInWater() && !StatusEffectUtil.hasWaterBreathing((LivingEntity) (Object) this) && !isInvulnerableTo(DamageSource.DROWN)) {
+                        Vec3d vec3d = this.getVelocity();
+
+                        for (int i = 0; i < 8; ++i) {
+                            float f = this.random.nextFloat() - this.random.nextFloat();
+                            float g = this.random.nextFloat() - this.random.nextFloat();
+                            float h = this.random.nextFloat() - this.random.nextFloat();
+                            this.world.addParticle(ParticleTypes.BUBBLE, this.getX() + (double) f, this.getY() + (double) g, this.getZ() + (double) h, vec3d.x, vec3d.y, vec3d.z);
+                        }
+                    }
+                }
+
+                this.damage(GalacticraftDamageSource.SUFFOCATION, 2.0F);
+            }
         }
+    }
+
+    private void updateAir(Entity entity) {
+        //todo check for sealed space
+        if (entity instanceof PlayerEntity) {
+            FullFixedItemInv gearInventory = ((GCPlayerAccessor) entity).getGearInventory();
+            if (gearInventory.getInvStack(6).getItem() instanceof OxygenTankItem && ((gearInventory.getInvStack(6).getMaxDamage() - gearInventory.getInvStack(6).getDamage()) > 0)) {
+                gearInventory.getInvStack(6).setDamage(gearInventory.getInvStack(6).getDamage() + 1);
+                return;
+            } else if (gearInventory.getInvStack(7).getItem() instanceof OxygenTankItem && ((gearInventory.getInvStack(7).getMaxDamage() - gearInventory.getInvStack(7).getDamage()) > 0)) {
+                gearInventory.getInvStack(7).setDamage(gearInventory.getInvStack(7).getDamage() + 1);
+                return;
+            }
+        }
+        entity.setAir(entity.getAir() - 1);
     }
 }
