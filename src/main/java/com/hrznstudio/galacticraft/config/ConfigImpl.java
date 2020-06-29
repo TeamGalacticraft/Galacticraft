4d9b1ec2e52a1bca6fcd223884dc574e10526fb1
Joe van der Zwet <me@joezwet.me>
2020-06-29 22:26:51 +1200

    chore(copyright): update year (6 months into 2020)

--- src/main/java/com/hrznstudio/galacticraft/config/ConfigImpl.java
+++ src/main/java/com/hrznstudio/galacticraft/config/ConfigImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 HRZN LTD
+ * Copyright (c) 2020 HRZN LTD
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -18,6 +18,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
  */
 
 package com.hrznstudio.galacticraft.config;
