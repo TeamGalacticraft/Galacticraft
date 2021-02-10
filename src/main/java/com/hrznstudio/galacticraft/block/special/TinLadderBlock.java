6d87daf9fb75e7756e35c9556345c2905e3f82ec
Brendan Lewis <brendanelewis@outlook.com>
2021-02-09 23:10:05 -0500

    Chore: update licenses

--- src/main/java/com/hrznstudio/galacticraft/block/special/TinLadderBlock.java
+++ src/main/java/com/hrznstudio/galacticraft/block/special/TinLadderBlock.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 HRZN LTD
+ * Copyright (c) 2019-2021 HRZN LTD
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
