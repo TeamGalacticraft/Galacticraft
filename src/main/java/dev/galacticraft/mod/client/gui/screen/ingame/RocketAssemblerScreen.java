243864889668a258d6386df9e7e081974c941b4e
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2021-05-04 19:59:56 -0700

    fix: remove debug code

--- src/main/java/dev/galacticraft/mod/client/gui/screen/ingame/RocketAssemblerScreen.java
+++ src/main/java/dev/galacticraft/mod/client/gui/screen/ingame/RocketAssemblerScreen.java
@@ -241,7 +241,6 @@ public class RocketAssemblerScreen extends HandledScreen<RocketAssemblerScreenHa
                         }
                         matrices.push();
                         matrices.translate(this.x + 13, this.y + 13 + ((GREEN_BOX_HEIGHT + 2) * baOY), 0);
-                        matrices.scale(50, 50, 50);
                         RocketPartRendererRegistry.getRenderer(blockEntity.data.getPartForType(RocketPartType.values()[i]).getId()).renderGUI(client.world, matrices, mouseX, mouseY, delta);
                         matrices.pop();
                     }
