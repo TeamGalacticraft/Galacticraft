fc5854e9092651e58a80a02576e34977664d9ffb
Brendan Lewis <brendanelewis@gmail.com>
2021-07-12 02:07:12 -0400

    Chore: remove space

--- src/main/java/dev/galacticraft/mod/client/render/entity/MoonVillagerEntityRenderer.java
+++ src/main/java/dev/galacticraft/mod/client/render/entity/MoonVillagerEntityRenderer.java
@@ -64,7 +64,6 @@ public class MoonVillagerEntityRenderer extends MobEntityRenderer<MoonVillagerEn
         } else {
             this.shadowRadius = 0.5F;
         }
-
         matrixStack.scale(g, g, g);
     }
 }
