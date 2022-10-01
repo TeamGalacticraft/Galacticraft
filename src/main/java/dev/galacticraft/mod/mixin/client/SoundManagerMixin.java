00df4e61a8c3d7d904c7a7fdd595825ec19f104d
alpha <alphamodeyt@gmail.com>
2022-10-01 16:56:52 -0500

    feat: allow sound to play in breathable environments

--- src/main/java/dev/galacticraft/mod/mixin/client/SoundManagerMixin.java
+++ src/main/java/dev/galacticraft/mod/mixin/client/SoundManagerMixin.java
@@ -4,6 +4,7 @@ import dev.galacticraft.mod.item.FrequencyModuleItem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,9 +15,11 @@ public class SoundManagerMixin {
     @Inject(method = "play", at = @At("HEAD"), cancellable = true)
     private void galacticraft$play(SoundInstance soundInstance, CallbackInfo ci) {
         if (soundInstance.getAttenuation() != SoundInstance.Attenuation.NONE) {
-            boolean hasModule = Minecraft.getInstance().player.getGearInv().hasAnyMatching(itemStack -> itemStack.getItem() instanceof FrequencyModuleItem);
-            if (!hasModule)
-                ci.cancel();
+            Player player = Minecraft.getInstance().player;
+            boolean hasModule = player.getGearInv().hasAnyMatching(itemStack -> itemStack.getItem() instanceof FrequencyModuleItem);
+            if (!Minecraft.getInstance().level.isBreathable(player.blockPosition()))
+                if (!hasModule)
+                    ci.cancel();
         }
     }
 }
