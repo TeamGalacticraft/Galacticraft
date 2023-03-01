fa4eb31282941d237109c5453a5cb8f6bc3df2cd
Sebastian B <me@sebastianb.dev>
2023-02-28 23:32:00 -0500

    feat: grey follows player with local gravity attribute

--- src/main/java/dev/galacticraft/mod/content/entity/GreyEntity.java
+++ src/main/java/dev/galacticraft/mod/content/entity/GreyEntity.java
@@ -25,29 +25,55 @@ package dev.galacticraft.mod.content.entity;
 import dev.galacticraft.api.entity.attribute.GcApiEntityAttributes;
 import dev.galacticraft.mod.content.entity.goals.FollowPlayerGoal;
 import net.minecraft.world.SimpleContainer;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.npc.InventoryCarrier;
 import net.minecraft.world.entity.npc.Npc;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
 public class GreyEntity extends PathfinderMob implements InventoryCarrier, Npc {
+
+    static class ForcePlayerLookGoal extends Goal {
+
+        Mob mob;
+        public ForcePlayerLookGoal(Mob mob) {
+            this.mob = mob;
+        }
+
+        @Override
+        public void tick() {
+            try {
+                mob.lookAt(mob.level.getNearestPlayer(mob, 20), 90F, 90F);
+            } catch (NullPointerException ignored) {
+
+            }
+        }
+
+        @Override
+        public boolean canUse() {
+            return true;
+        }
+    }
     public GreyEntity(EntityType<? extends PathfinderMob> entityType, Level level) {
         super(entityType, level);
     }
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(0, new RandomLookAroundGoal(this));
-        this.goalSelector.addGoal(1, new FollowPlayerGoal(this, 7.0F, 1));
-        this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 6F));
+        // this.goalSelector.addGoal(0, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(0, new ForcePlayerLookGoal(this));
+        this.goalSelector.addGoal(1, new FollowPlayerGoal(this, 7.0F, 0.8));
+
     }
+
+    @Override
+    public void tick() {
+        super.tick();
+    }
+
 
     @Override
     public SimpleContainer getInventory() {
@@ -60,6 +86,7 @@ public class GreyEntity extends PathfinderMob implements InventoryCarrier, Npc {
                 .add(Attributes.FOLLOW_RANGE, 12.0)
                 .add(Attributes.MAX_HEALTH, 24.0)
                 .add(Attributes.ATTACK_DAMAGE, 5.0)
-                .add(GcApiEntityAttributes.CAN_BREATHE_IN_SPACE, 1.0D);
+                .add(GcApiEntityAttributes.CAN_BREATHE_IN_SPACE, 1.0D)
+                .add(GcApiEntityAttributes.LOCAL_GRAVITY_LEVEL, 0.9F);
     }
 }
