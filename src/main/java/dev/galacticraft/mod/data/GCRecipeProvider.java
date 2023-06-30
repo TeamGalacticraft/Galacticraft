9703f043533136b16ea273efbd91827de73012ec
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2023-06-30 10:48:10 -0400

    refactor: Generate nugget recipes via data gen

--- src/main/java/dev/galacticraft/mod/data/GCRecipeProvider.java
+++ src/main/java/dev/galacticraft/mod/data/GCRecipeProvider.java
@@ -34,6 +34,7 @@ import net.fabricmc.fabric.api.tag.convention.v1.ConventionalItemTags;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 
@@ -187,7 +188,7 @@ public class GCRecipeProvider extends FabricRecipeProvider {
         CircuitFabricatorRecipeBuilder.create(GCItems.SOLAR_ARRAY_WAFER, 3)
                 .requires(GCItems.SOLAR_DUST)
                 .save(exporter);
-        
+
         // Nuggets to Ingots
         build3x3Compression(exporter, GCItems.METEORIC_IRON_NUGGET, GCItems.METEORIC_IRON_INGOT);
         build3x3Compression(exporter, GCItems.DESH_NUGGET, GCItems.DESH_INGOT);
@@ -195,6 +196,14 @@ public class GCRecipeProvider extends FabricRecipeProvider {
         build3x3Compression(exporter, GCItems.ALUMINUM_NUGGET, GCItems.ALUMINUM_INGOT);
         build3x3Compression(exporter, GCItems.TIN_NUGGET, GCItems.TIN_INGOT);
         build3x3Compression(exporter, GCItems.TITANIUM_NUGGET, GCItems.TITANIUM_INGOT);
+        
+        // Nuggets <-> Ingots
+        buildExpansionCompressionPair(exporter, GCItems.METEORIC_IRON_NUGGET, GCItems.METEORIC_IRON_INGOT);
+        buildExpansionCompressionPair(exporter, GCItems.DESH_NUGGET, GCItems.DESH_INGOT);
+        buildExpansionCompressionPair(exporter, GCItems.LEAD_NUGGET, GCItems.LEAD_INGOT);
+        buildExpansionCompressionPair(exporter, GCItems.ALUMINUM_NUGGET, GCItems.ALUMINUM_INGOT);
+        buildExpansionCompressionPair(exporter, GCItems.TIN_NUGGET, GCItems.TIN_INGOT);
+        buildExpansionCompressionPair(exporter, GCItems.TITANIUM_NUGGET, GCItems.TITANIUM_INGOT);
     }
 
     private void build3x3Compression(Consumer<FinishedRecipe> exporter, Item input, Item result) {
@@ -206,4 +215,17 @@ public class GCRecipeProvider extends FabricRecipeProvider {
                 .unlockedBy(getHasName(input), has(input))
                 .save(exporter);
     }
+
+    private void build9Expansion(Consumer<FinishedRecipe> exporter, Item input, Item result) {
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, result, 9)
+                .requires(input, 1)
+                .unlockedBy(getHasName(input), has(input))
+                .save(exporter);
+    }
+
+    private void buildExpansionCompressionPair(Consumer<FinishedRecipe> exporter, Item expansionVariant, Item compressedVariant) {
+        build3x3Compression(exporter, expansionVariant, compressedVariant);
+        build9Expansion(exporter, compressedVariant, expansionVariant);
+    }
+
 }
