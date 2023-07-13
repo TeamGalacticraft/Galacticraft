2d42368e56317329357475c3884e060c6530e9b3
Wasinthorn Suksri <mccommander_minecraft@hotmail.com>
2023-07-14 05:35:50 +0700

    feat: added torches to wall_post_override tag
    (cherry picked from commit cde5195f4752f2421c120c26130d4759177d12ca)

--- src/main/java/dev/galacticraft/mod/data/tag/GCBlockTagProvider.java
+++ src/main/java/dev/galacticraft/mod/data/tag/GCBlockTagProvider.java
@@ -22,15 +22,12 @@
 
 package dev.galacticraft.mod.data.tag;
 
-import dev.galacticraft.mod.Constant;
 import dev.galacticraft.mod.content.GCBlocks;
 import dev.galacticraft.mod.tag.GCTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.level.block.Block;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -89,6 +86,9 @@ public class GCBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .add(this.reverseLookup(GCBlocks.COBBLED_MOON_ROCK_WALL))
                 .add(this.reverseLookup(GCBlocks.MOON_BASALT_WALL))
                 .add(this.reverseLookup(GCBlocks.MOON_BASALT_BRICK_WALL));
-        ;
+
+        this.tag(BlockTags.WALL_POST_OVERRIDE)
+                .add(this.reverseLookup(GCBlocks.GLOWSTONE_TORCH))
+                .add(this.reverseLookup(GCBlocks.UNLIT_TORCH));
     }
 }
