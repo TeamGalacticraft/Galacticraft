c3e1d0f53ea400a01df522c0139b758d1b118cac
JustAPotota <justapotota.dev@gmail.com>
2024-02-24 16:22:43 -0600

    feat: make hot meteor chunk items cool in water

--- src/main/java/dev/galacticraft/mod/mixin/ItemEntityMixin.java
+++ src/main/java/dev/galacticraft/mod/mixin/ItemEntityMixin.java
@@ -3,8 +3,11 @@ package dev.galacticraft.mod.mixin;
 import dev.galacticraft.mod.content.item.GCItems;
 import dev.galacticraft.mod.content.item.HotThrowableMeteorChunkItem;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +15,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
-public abstract class ItemEntityMixin {
+public abstract class ItemEntityMixin extends Entity {
+    public ItemEntityMixin(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
+
     @Shadow public abstract ItemStack getItem();
 
     @Shadow public abstract void setItem(ItemStack stack);
@@ -21,12 +28,19 @@ public abstract class ItemEntityMixin {
     void coolMeteorChunk(CallbackInfo ci) {
         ItemStack stack = this.getItem();
         CompoundTag tag = stack.getTag();
-        if (stack.is(GCItems.HOT_THROWABLE_METEOR_CHUNK) && tag != null) {
-            int ticksUntilCool = tag.getInt(HotThrowableMeteorChunkItem.TICKS_UNTIL_COOL);
-            if (ticksUntilCool == 0) {
-                this.setItem(new ItemStack(GCItems.THROWABLE_METEOR_CHUNK, stack.getCount()));
+        if (stack.is(GCItems.HOT_THROWABLE_METEOR_CHUNK)) {
+            if (tag != null) {
+                int ticksUntilCool = tag.getInt(HotThrowableMeteorChunkItem.TICKS_UNTIL_COOL);
+                if (ticksUntilCool == 0) {
+                    this.setItem(new ItemStack(GCItems.THROWABLE_METEOR_CHUNK, stack.getCount()));
+                }
+                tag.putInt(HotThrowableMeteorChunkItem.TICKS_UNTIL_COOL, ticksUntilCool - 1);
             }
-            tag.putInt(HotThrowableMeteorChunkItem.TICKS_UNTIL_COOL, ticksUntilCool - 1);
+
+            if (this.isInWater()) {
+                this.setItem(new ItemStack(GCItems.THROWABLE_METEOR_CHUNK, stack.getCount()));
+                this.playEntityOnFireExtinguishedSound();
+            }
         }
     }
 }
