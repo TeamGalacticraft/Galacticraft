26e65df293dc1868ab2ba7d972d7b6d2ac72a824
Roelymole <roelymole@gmail.com>
2025-02-10 11:55:12 +0000

    fix: change the bubbles progress indicator in the oxygen (de)compressor GUI in discrete steps

--- src/main/java/dev/galacticraft/mod/client/gui/screen/ingame/OxygenCompressorScreen.java
+++ src/main/java/dev/galacticraft/mod/client/gui/screen/ingame/OxygenCompressorScreen.java
@@ -49,10 +49,8 @@ public class OxygenCompressorScreen extends MachineScreen<OxygenCompressorBlockE
     protected void renderMachineBackground(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
         super.renderMachineBackground(graphics, mouseX, mouseY, delta);
         if (this.menu.state.isActive()) {
-            double height = (System.currentTimeMillis() % 2250);
-            if (height == 0) return; //prevent dividing by zero
-            height /= -125.0;
-            DrawableUtil.drawProgressTexture(graphics.pose(), this.leftPos + 93, this.topPos + 64, 187, 18, -11, (float) height);
+            int height = -(int) (System.currentTimeMillis() % 2250) / 125;
+            DrawableUtil.drawProgressTexture(graphics.pose(), this.leftPos + 93, this.topPos + 64, 187, 18, -11, height);
         }
     }
 }
