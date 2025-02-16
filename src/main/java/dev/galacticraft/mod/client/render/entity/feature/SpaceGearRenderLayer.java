d8d904b15e67a33d84be9b45550fa70f55d681c2
Roelymole <roelymole@gmail.com>
2025-02-16 11:44:57 +0000

    refactor: use the same texture for the left/right tanks

--- src/main/java/dev/galacticraft/mod/client/render/entity/feature/SpaceGearRenderLayer.java
+++ src/main/java/dev/galacticraft/mod/client/render/entity/feature/SpaceGearRenderLayer.java
@@ -55,15 +55,15 @@ public class SpaceGearRenderLayer<T extends LivingEntity, M extends EntityModel<
     private static final ResourceLocation TEXTURE = Constant.id("textures/entity/oxygen_gear.png");
     private final @Nullable ModelPart mask;
     private final @Nullable ModelPart pipe;
-    private @Nullable ModelPart root;
+    private final @Nullable ModelPart tank;
 
     public SpaceGearRenderLayer(RenderLayerParent<T, M> context) {
         super(context);
-        ModelPart head, body;
+        ModelPart root, head, body;
         if (context.getModel() instanceof HierarchicalModel<?> model) {
-            this.root = model.root();
-            head = this.root.getChild(PartNames.HEAD);
-            body = this.root.getChild(PartNames.BODY);
+            root = model.root();
+            head = root.getChild(PartNames.HEAD);
+            body = root.getChild(PartNames.BODY);
         } else if (context.getModel() instanceof HumanoidModel<?> model){
             head = model.head;
             body = model.body;
@@ -73,7 +73,7 @@ public class SpaceGearRenderLayer<T extends LivingEntity, M extends EntityModel<
         } else {
             this.mask = null;
             this.pipe = null;
-            this.root = null;
+            this.tank = null;
             return;
         }
         MeshDefinition modelData = new MeshDefinition();
@@ -84,28 +84,26 @@ public class SpaceGearRenderLayer<T extends LivingEntity, M extends EntityModel<
 
         if (body != null) {
             modelPartData.addOrReplaceChild(Constant.ModelPartName.OXYGEN_PIPE, CubeListBuilder.create().texOffs(40, 7).addBox(-2.0F, -3.0F, 0.0F, 4, 5, 8, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
-            modelPartData.addOrReplaceChild("small_left", CubeListBuilder.create().texOffs(0, 20).addBox(0.0F, 1.0F, 2.0F, 4, 6, 4, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
-            modelPartData.addOrReplaceChild("small_right", CubeListBuilder.create().texOffs(16, 20).addBox(-4.0F, 1.0F, 2.0F, 4, 6, 4, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
-            modelPartData.addOrReplaceChild("medium_left", CubeListBuilder.create().texOffs(32, 20).addBox(0.0F, 1.0F, 2.0F, 4, 6, 4, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
-            modelPartData.addOrReplaceChild("medium_right", CubeListBuilder.create().texOffs(48, 20).addBox(-4.0F, 1.0F, 2.0F, 4, 6, 4, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
-            modelPartData.addOrReplaceChild("large_left", CubeListBuilder.create().texOffs(0, 32).addBox(0.0F, 1.0F, 2.0F, 4, 6, 4, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
-            modelPartData.addOrReplaceChild("large_right", CubeListBuilder.create().texOffs(16, 32).addBox(-4.0F, 1.0F, 2.0F, 4, 6, 4, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
-            modelPartData.addOrReplaceChild("infinite_left", CubeListBuilder.create().texOffs(32, 32).addBox(0.0F, 1.0F, 2.0F, 4, 6, 4, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
-            modelPartData.addOrReplaceChild("infinite_right", CubeListBuilder.create().texOffs(48, 32).addBox(-4.0F, 1.0F, 2.0F, 4, 6, 4, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
+            modelPartData.addOrReplaceChild(Constant.Item.SMALL_OXYGEN_TANK, CubeListBuilder.create().texOffs(0, 20).addBox(0.0F, 1.0F, 2.01F, 4, 8, 4, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
+            modelPartData.addOrReplaceChild(Constant.Item.MEDIUM_OXYGEN_TANK, CubeListBuilder.create().texOffs(16, 20).addBox(0.0F, 1.0F, 2.01F, 4, 8, 4, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
+            modelPartData.addOrReplaceChild(Constant.Item.LARGE_OXYGEN_TANK, CubeListBuilder.create().texOffs(32, 20).addBox(0.0F, 1.0F, 2.01F, 4, 8, 4, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
+            modelPartData.addOrReplaceChild(Constant.Item.INFINITE_OXYGEN_TANK, CubeListBuilder.create().texOffs(48, 20).addBox(0.0F, 1.0F, 2.01F, 4, 8, 4, CubeDeformation.NONE), PartPose.offset(body.x, body.y, body.z));
         }
 
-        this.root = modelPartData.bake(64, 64);
+        root = modelPartData.bake(64, 32);
 
         if (head != null) {
-            this.mask = this.root.getChild(Constant.ModelPartName.OXYGEN_MASK);
+            this.mask = root.getChild(Constant.ModelPartName.OXYGEN_MASK);
         } else {
             this.mask = null;
         }
 
         if (body != null) {
-            this.pipe = this.root.getChild(Constant.ModelPartName.OXYGEN_PIPE);
+            this.pipe = root.getChild(Constant.ModelPartName.OXYGEN_PIPE);
+            this.tank = root;
         } else {
             this.pipe = null;
+            this.tank = null;
         }
     }
 
@@ -117,46 +115,20 @@ public class SpaceGearRenderLayer<T extends LivingEntity, M extends EntityModel<
         boolean hasGear = true;
         boolean hasTank1 = true;
         boolean hasTank2 = true;
-        String tankSize1 = "medium_left";
-        String tankSize2 = "medium_right";
+        String tankSize1 = Constant.Item.MEDIUM_OXYGEN_TANK;
+        String tankSize2 = Constant.Item.MEDIUM_OXYGEN_TANK;
 
-        if (livingEntity instanceof Player) {
+        if (livingEntity instanceof Player player) {
             Container inv = livingEntity.galacticraft$getGearInv();
             hasMask = inv.getItem(GCAccessorySlots.OXYGEN_MASK_SLOT).is(GCTags.OXYGEN_MASKS);
             hasGear = inv.getItem(GCAccessorySlots.OXYGEN_GEAR_SLOT).is(GCTags.OXYGEN_GEAR);
             hasTank1 = inv.getItem(GCAccessorySlots.OXYGEN_TANK_1_SLOT).is(GCTags.OXYGEN_TANKS);
             hasTank2 = inv.getItem(GCAccessorySlots.OXYGEN_TANK_2_SLOT).is(GCTags.OXYGEN_TANKS);
-            switch (inv.getItem(GCAccessorySlots.OXYGEN_TANK_1_SLOT).getDescriptionId().replace("item.galacticraft.", "")) {
-                case Constant.Item.SMALL_OXYGEN_TANK:
-                    tankSize1 = "small_left";
-                    break;
-                case Constant.Item.MEDIUM_OXYGEN_TANK:
-                    tankSize1 = "medium_left";
-                    break;
-                case Constant.Item.LARGE_OXYGEN_TANK:
-                    tankSize1 = "large_left";
-                    break;
-                case Constant.Item.INFINITE_OXYGEN_TANK:
-                    tankSize1 = "infinite_left";
-                    break;
-                default:
-                    tankSize1 = "medium_left";
+            if (hasTank1) {
+                tankSize1 = inv.getItem(GCAccessorySlots.OXYGEN_TANK_1_SLOT).getDescriptionId().replace("item.galacticraft.", "");
             }
-            switch (inv.getItem(GCAccessorySlots.OXYGEN_TANK_2_SLOT).getDescriptionId().replace("item.galacticraft.", "")) {
-                case Constant.Item.SMALL_OXYGEN_TANK:
-                    tankSize2 = "small_right";
-                    break;
-                case Constant.Item.MEDIUM_OXYGEN_TANK:
-                    tankSize2 = "medium_right";
-                    break;
-                case Constant.Item.LARGE_OXYGEN_TANK:
-                    tankSize2 = "large_right";
-                    break;
-                case Constant.Item.INFINITE_OXYGEN_TANK:
-                    tankSize2 = "infinite_right";
-                    break;
-                default:
-                    tankSize2 = "medium_right";
+            if (hasTank2) {
+                tankSize2 = inv.getItem(GCAccessorySlots.OXYGEN_TANK_2_SLOT).getDescriptionId().replace("item.galacticraft.", "");
             }
         } else if (livingEntity instanceof Zombie) {
             Zombie zombie = (Zombie) entity;
@@ -174,11 +146,12 @@ public class SpaceGearRenderLayer<T extends LivingEntity, M extends EntityModel<
         if (this.pipe != null && hasGear) {
             this.pipe.render(matrices, vertexConsumer, light, OverlayTexture.NO_OVERLAY);
         }
-        if (this.root != null && hasTank1) {
-            this.root.getChild(tankSize1).render(matrices, vertexConsumer, light, OverlayTexture.NO_OVERLAY);
+        if (this.tank != null && hasTank1) {
+            this.tank.getChild(tankSize1).render(matrices, vertexConsumer, light, OverlayTexture.NO_OVERLAY);
         }
-        if (this.root != null && hasTank2) {
-            this.root.getChild(tankSize2).render(matrices, vertexConsumer, light, OverlayTexture.NO_OVERLAY);
+        if (this.tank != null && hasTank2) {
+            matrices.translate(-0.25F, 0.0F, 0.0F);
+            this.tank.getChild(tankSize2).render(matrices, vertexConsumer, light, OverlayTexture.NO_OVERLAY);
         }
     }
 
