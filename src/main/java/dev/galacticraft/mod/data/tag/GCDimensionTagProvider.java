040fded9948d8a67044fb10e8f0a393d801ae8b5
Roelymole <roelymole@gmail.com>
2025-03-16 11:48:37 +0000

    refactor: change GCTags to GCDimensionTypeTags

--- src/main/java/dev/galacticraft/mod/data/tag/GCDimensionTagProvider.java
+++ src/main/java/dev/galacticraft/mod/data/tag/GCDimensionTagProvider.java
@@ -45,7 +45,7 @@ public class GCDimensionTagProvider extends FabricTagProvider<DimensionType> {
         this.tag(GCDimensionTypeTags.SPACE)
                 .add(GCDimensionTypes.MOON)
                 .add(GCDimensionTypes.VENUS);
-        tag(GCTags.VACUUM)
+        tag(GCDimensionTypeTags.VACUUM)
                 .add(GCDimensionTypes.MOON);
     }
 }
