c9948081f3f671369b1b09d36014ed78a78f0705
scott <scott_roxburgh@hotmail.com>
2025-08-12 13:22:05 +1200

    Removed unused import.

--- src/main/java/dev/galacticraft/mod/content/block/special/TinLadderBlock.java
+++ src/main/java/dev/galacticraft/mod/content/block/special/TinLadderBlock.java
@@ -29,7 +29,6 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
