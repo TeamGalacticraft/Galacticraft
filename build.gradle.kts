844acde5a652699445a53685a2b0c23f01da5c27
maxryan008 <maxryan008@gmail.com>
2025-08-12 19:07:06 +1000

    Revert "Changed spotless"
    This reverts commit a709406bf9b8a1ee09a7f98742b74d37973b578b.

--- build.gradle.kts
+++ build.gradle.kts
@@ -485,11 +485,8 @@ tasks.test {
 
 spotless {
     lineEndings = com.diffplug.spotless.LineEnding.UNIX
-    ratchetFrom("origin/main")            // only check diffs in main
 
     java {
-        target("src/**/*.java")
-        targetExclude("src/main/generated/**", "**/build/**")
         licenseHeader(processLicenseHeader(rootProject.file("LICENSE")))
         leadingTabsToSpaces()
         removeUnusedImports()
