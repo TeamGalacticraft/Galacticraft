bdf033a24682535693346c6d972e77e968c3354c
Roelymole <roelymole@gmail.com>
2025-08-14 00:07:24 +0100

    chore: remove unused import

--- src/main/java/dev/galacticraft/mod/content/entity/vehicle/Buggy.java
+++ src/main/java/dev/galacticraft/mod/content/entity/vehicle/Buggy.java
@@ -46,7 +46,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.phys.Vec3;
 
 import java.util.function.IntFunction;
 
