b7c2fead01d7354219ad559e61592b745c6ff083
scott <scott_roxburgh@hotmail.com>
2025-08-21 10:56:20 +1200

    Handles water logged fluids consistently.

--- src/main/java/dev/galacticraft/mod/content/block/special/TinLadderBlock.java
+++ src/main/java/dev/galacticraft/mod/content/block/special/TinLadderBlock.java
@@ -38,7 +38,6 @@ import net.minecraft.world.level.block.LadderBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
-import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
