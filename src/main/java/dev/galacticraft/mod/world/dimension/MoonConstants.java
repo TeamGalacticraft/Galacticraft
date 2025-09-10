172cda9758f7a4a6507959a59e141e47cd6ed191
maxryan008 <maxryan008@gmail.com>
2025-09-10 12:35:07 +1000

    Forgot to push moon constants change

--- src/main/java/dev/galacticraft/mod/world/dimension/MoonConstants.java
+++ src/main/java/dev/galacticraft/mod/world/dimension/MoonConstants.java
@@ -22,35 +22,50 @@
 
 package dev.galacticraft.mod.world.dimension;
 
-public class MoonConstants {
-    public static final int MOON_MIN_TERRAIN_HEIGHT = -128;
-    public static final int MOON_MAX_HEIGHT = 384;
+public interface MoonConstants {
+    interface Dimension {
+        int MIN_DIMENSION_HEIGHT = -64;
+        int MAX_DIMENSION_HEIGHT = 384;
+    }
 
-    public static final int COMET_TUNDRA_SURFACE_HEIGHT = 60;
-    public static final int BASALTIC_MARE_SURFACE_HEIGHT = 60;
-    public static final int LUNAR_LOW_LANDS_SURFACE_HEIGHT = 80;
-    public static final int LUNAR_HIGH_LANDS_SURFACE_HEIGHT = 140;
+    interface SurfaceHeights {
+        int COMET_TUNDRA = 60;
+        int BASALTIC_MARE = 60;
+        int LUNAR_LOW_LANDS = 80;
+        int LUNAR_HIGH_LANDS = 140;
+    }
 
-    public static final int COMET_TUNDRA_SURFACE_HEIGHT_VARIATION = 3;
-    public static final int BASALTIC_MARE_SURFACE_HEIGHT_VARIATION = 3;
-    public static final int LUNAR_LOW_LANDS_SURFACE_HEIGHT_VARIATION = 20;
-    public static final int LUNAR_HIGH_LANDS_SURFACE_HEIGHT_VARIATION = 3;
+    interface HeightVariations {
+        int COMET_TUNDRA = 3;
+        int BASALTIC_MARE = 3;
+        int LUNAR_LOW_LANDS = 20;
+        int LUNAR_HIGH_LANDS = 2;
+    }
 
-    public static final float COMET_TUNDRA_CONTINENTALNESS_MINIMUM = -1.2f;
-    public static final float BASALTIC_MARE_CONTINENTALNESS_MINIMUM = -0.455f;
-    public static final float LUNAR_LOW_LANDS_CONTINENTALNESS_MINIMUM = -0.11f;
-    public static final float LUNAR_HIGH_LANDS_CONTINENTALNESS_MINIMUM = 0.3f;
+    interface MinimumContinentalness {
+        float COMET_TUNDRA = -1.2f;
+        float BASALTIC_MARE = -0.455f;
+        float LUNAR_LOW_LANDS = -0.11f;
+        float LUNAR_HIGH_LANDS = 0.3f;
+    }
 
-    public static final float LUNAR_LOW_TO_HIGH_LANDS_RANGE = 0.05f;
-    public static final float BASALTIC_MARE_TO_LUNAR_LOW_LANDS_RANGE = 0.1f;
+    interface Ranges {
+        float LOW_TO_HIGH = 0.05f;
+        float BASALTIC_TO_LOW = 0.1f;
+    }
 
-    // --OLIVINE CAVES--
-    public static final float OLIVINE_CAVE_PROBABILITY = 0.25f;
-    public static final int OLIVINE_CAVE_MIN_HEIGHT = -68;
-    public static final int OLIVINE_CAVE_MAX_HEIGHT = 30;
-    public static final float OLIVINE_CAVE_Y_SCALE_MIN = 0.8f;
-    public static final float OLIVINE_CAVE_Y_SCALE_MAX = 2.2f;
-    public static final float OLIVINE_CAVE_BASALT_INTERIOR_CHANCE = 0.95f; //95%
-    public static final int MAX_FEATURE_SPAWN = 55;
-    public static final int MIN_FEATURE_SPAWN = -68; //95%
+    interface OlivineCaves {
+        float PROBABILITY = 0.25f;
+        int MAX_HEIGHT = 30;
+        int MIN_HEIGHT = -30;
+        float Y_SCALE_MAX = 2.2f;
+        float Y_SCALE_MIN = 0.8f;
+        float BASALT_INTERIOR_CHANCE = 0.95f;
+        int MAX_FEATURE_SPAWN = 55;
+        int MIN_FEATURE_SPAWN = -30;
+    }
+
+    interface LunarHighLands {
+        double NOISE_SCALE_XZ = 3;
+    }
 }
