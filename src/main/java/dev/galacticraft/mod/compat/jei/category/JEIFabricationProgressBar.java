d478db09d2c909f7f5f50982a9c96ba3839bc383
Roelymole <roelymole@gmail.com>
2025-09-20 16:33:17 +0100

    chore: add circuit fabricator progress animations to REI/JEI

---
+++ src/main/java/dev/galacticraft/mod/compat/jei/category/JEIFabricationProgressBar.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2019-2025 Team Galacticraft
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.galacticraft.mod.compat.jei.category;
+
+import dev.galacticraft.mod.client.gui.CircuitFabricatorProgressAnimation;
+import mezz.jei.api.gui.drawable.IDrawable;
+import net.minecraft.client.gui.GuiGraphics;
+
+import static dev.galacticraft.mod.Constant.RecipeViewer.*;
+
+public class JEIFabricationProgressBar implements IDrawable {
+    private int processingTime = 1;
+    private long startTime;
+
+    public void setProcessingTime(int processingTime) {
+        this.processingTime = processingTime * 50;
+        this.startTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public void draw(GuiGraphics graphics, int xOffset, int yOffset) {
+        float progress = ((System.currentTimeMillis() - this.startTime) % this.processingTime) / (float) this.processingTime;
+        CircuitFabricatorProgressAnimation.render(graphics, xOffset, yOffset, progress);
+    }
+}
---
+++ src/main/java/dev/galacticraft/mod/compat/rei/client/category/FabricationProgressWidget.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2019-2025 Team Galacticraft
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.galacticraft.mod.compat.rei.client.category;
+
+import dev.galacticraft.mod.client.gui.CircuitFabricatorProgressAnimation;
+import me.shedaniel.rei.api.client.gui.widgets.Widget;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.List;
+
+import static dev.galacticraft.mod.Constant.RecipeViewer.*;
+
+public class FabricationProgressWidget extends Widget {
+    private final int x;
+    private final int y;
+    private final int processingTime;
+
+    public FabricationProgressWidget(int x, int y, int processingTime) {
+        this.x = x;
+        this.y = y;
+        this.processingTime = processingTime * 50;
+    }
+
+    @Override
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+        float progress = (System.currentTimeMillis() % this.processingTime) / (float) this.processingTime;
+        CircuitFabricatorProgressAnimation.render(graphics, this.x, this.y, progress);
+    }
+
+    @Override
+    public List<? extends GuiEventListener> children() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isDragging() {
+        return false;
+    }
+
+    @Override
+    public void setDragging(boolean dragging) {
+
+    }
+
+    @Nullable
+    @Override
+    public GuiEventListener getFocused() {
+        return null;
+    }
+
+    @Override
+    public void setFocused(@Nullable GuiEventListener focused) {
+
+    }
+}
