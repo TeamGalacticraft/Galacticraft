5327d8fba4df4d4920eea2200fd67aa98423e269
Mason Rohner <rohnermason@gmail.com>
2026-03-05 10:48:04 -0700

    feat: added MachineSoundInstance class

---
+++ src/main/java/dev/galacticraft/mod/client/sounds/MachineSoundInstance.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2019-2026 Team Galacticraft
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.galacticraft.mod.client.sounds;
+
+
+
+import dev.galacticraft.machinelib.api.block.entity.MachineBlockEntity;
+import dev.galacticraft.mod.content.GCSounds;
+import net.minecraft.client.resources.sounds.AbstractTickableSoundInstance;
+import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+
+public class MachineSoundInstance extends AbstractTickableSoundInstance {
+    protected final MachineBlockEntity machine;
+
+    public MachineSoundInstance(MachineBlockEntity entity, SoundEvent sound, SoundSource source) {
+        super(GCSounds.MACHINE_BUZZ, SoundSource.BLOCKS,SoundInstance.createUnseededRandom());
+        this.machine = entity;
+        this.looping = true;
+        this.delay = 0;
+        this.volume = 0.0F;
+    }
+    
+    public void tick() {
+        if (!machine.energyStorage().isEmpty()) {
+            this.volume = 1.0F;
+
+        } else {
+            this.stop();
+        }
+
+    }
+}
