20f786d7f35e39fc808e305007c4a836d0cbdd62
JustAPotota <justapotota.dev@gmail.com>
2026-04-04 21:58:39 -0500

    fix: disable elytra rendering while in cryo chamber

---
+++ src/main/java/dev/galacticraft/mod/mixin/client/ElytraLayerMixin.java
@@ -0,0 +1,20 @@
+package dev.galacticraft.mod.mixin.client;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.layers.ElytraLayer;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ElytraLayer.class)
+public abstract class ElytraLayerMixin<T extends LivingEntity> {
+    @Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V", at = @At("HEAD"), cancellable = true)
+    public void galacticraft$disableElytraInCryoChamber(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, T livingEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
+        if (livingEntity.isInCryoSleep()) {
+            ci.cancel();
+        }
+    }
+}
