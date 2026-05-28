91164949046113a152a7cdf4f0ee6b72bef60b30
maxryan008 <maxryan008@gmail.com>
2026-05-28 15:56:16 +1000

    Add most of the blocks from feature/moon

---
+++ src/main/java/dev/galacticraft/mod/content/item/MoonTangleFruitItem.java
@@ -0,0 +1,9 @@
+package dev.galacticraft.mod.content.item;
+
+import net.minecraft.world.item.Item;
+
+public class MoonTangleFruitItem extends Item {
+    public MoonTangleFruitItem(Properties properties) {
+        super(properties);
+    }
+}
