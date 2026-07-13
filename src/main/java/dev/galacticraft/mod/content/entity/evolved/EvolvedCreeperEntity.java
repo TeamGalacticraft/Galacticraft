e012714d674006c4d5e34a1ee7586764e782de09
TakeABao23 <mrohner3@byu.edu>
2026-07-13 13:09:24 -0600

    style: spotlessApply

--- src/main/java/dev/galacticraft/mod/content/entity/evolved/EvolvedCreeperEntity.java
+++ src/main/java/dev/galacticraft/mod/content/entity/evolved/EvolvedCreeperEntity.java
@@ -28,8 +28,6 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Pose;
